59fccfe139efaa6e5f99ab531f39878059ee3782
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2021-02-22 00:26:01 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yuumi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'yuumi';
+}
